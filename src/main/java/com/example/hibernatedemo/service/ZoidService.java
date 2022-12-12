5dce66cf13ec4d16c7f302cc8fc5416aa11e84a4
Louis Conley <louismconley@gmail.com>
2022-12-11 18:03:42 -0800

    Created service layer using ZoidDAO CRUD methods

--- src/main/java/com/example/hibernatedemo/service/ZoidService.java
+++ src/main/java/com/example/hibernatedemo/service/ZoidService.java
@@ -1,4 +1,25 @@
 package com.example.hibernatedemo.service;
 
+import com.example.hibernatedemo.data.ZoidDAO;
+import com.example.hibernatedemo.model.Zoid;
+
 public class ZoidService {
+
+    private final ZoidDAO zoidDAO = new ZoidDAO();
+
+    public Zoid save(Zoid zoid) {
+        return zoidDAO.insert(zoid);
+    }
+
+    public Zoid update(Zoid zoid) {
+        return zoidDAO.update(zoid);
+    }
+
+    public Zoid getById(Long id) {
+        return zoidDAO.getById(id);
+    }
+
+    public void delete(Long id) {
+        zoidDAO.deleteById(id);
+    }
 }
